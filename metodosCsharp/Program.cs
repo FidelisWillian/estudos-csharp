a979cc84c9b09359e478b7c52d571fd86c0e73e5
Willian Fidelis <wwl.fidelis@gmail.com>
2026-01-25 13:22:45 -0300

    metodos no csharp

---
+++ metodosCsharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using metodosCsharp.models;
+
+Pessoa p = new Pessoa();
+p.nome = "Willian";
+p.sobrenome = "Fidelis";
+
+Pessoa p1 = new Pessoa();
+p1.nome = "Lorena";
+p1.sobrenome = "Camilli";
+
+Curso c = new Curso();
+c.Alunos = new List<Pessoa>();
+c.Nome = "Curso de C#";
+c.AdicionarAluno(p);
+c.AdicionarAluno(p1);
+c.ListarAlunos();
