077e4be4a79e2aa75c63762c9aa7cb645fa948ac
Willian Fidelis <wwl.fidelis@gmail.com>
2026-01-27 22:16:42 -0300

    Valores monetarios em C#

---
+++ valoresMonetarios/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+// Define a cultura do "sistema" inteiro como pt-BR
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+decimal valorMonetario = 1582.40M;
+
+// É possível utilizar o método "ToString" para formatar uma visualização em especifica, nesse caso o valor será exibido como dolár por conta da formatação da cultura em "en-US"
+System.Console.WriteLine(valorMonetario.ToString("C", CultureInfo.CreateSpecificCulture("en-US")));
